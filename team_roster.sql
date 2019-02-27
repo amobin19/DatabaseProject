64b7019d472c6b5e145d25886ed08bc8b22f90fd
anna-thilly <31319616+anna-thilly@users.noreply.github.com>
2019-02-27 15:26:12 -0500

    team_roster.sql upload

---
+++ team_roster.sql
@@ -0,0 +1,25 @@
+CREATE TABLE team_roster(
+   Player       VARCHAR(18) NOT NULL PRIMARY KEY
+  ,Number       INTEGER  NOT NULL
+  ,Class        VARCHAR(2) NOT NULL
+  ,Pos          VARCHAR(1) NOT NULL
+  ,Height       VARCHAR(4)
+  ,Weight       INTEGER 
+  ,Hometown     VARCHAR(22) NOT NULL
+  ,High_School  VARCHAR(33)
+);
+INSERT INTO team_roster(Player,Number,Class,Pos,Height,Weight,Hometown,High_School) VALUES ('De''Andre Hunter',12,'SO','G','6-7',225,'Philadelphia PA','Friends'' Central School');
+INSERT INTO team_roster(Player,Number,Class,Pos,Height,Weight,Hometown,High_School) VALUES ('Kyle Guy',5,'JR','G','6-2',175,'Indianapolis IN','Lawrence Central High School');
+INSERT INTO team_roster(Player,Number,Class,Pos,Height,Weight,Hometown,High_School) VALUES ('Ty Jerome',11,'JR','G','6-5',195,'New Rochelle NY','Iona Preparatory School');
+INSERT INTO team_roster(Player,Number,Class,Pos,Height,Weight,Hometown,High_School) VALUES ('Mamadi Diakite',25,'JR','F','6-9',228,'Conakry Guinea','Blue Ridge High School');
+INSERT INTO team_roster(Player,Number,Class,Pos,Height,Weight,Hometown,High_School) VALUES ('Braxton Key',2,'JR','G','6-8',225,'Charlotte NC','Oak Hill Academy');
+INSERT INTO team_roster(Player,Number,Class,Pos,Height,Weight,Hometown,High_School) VALUES ('Jay Huff',30,'SO','F','7-1',232,'Durham NC','Voyager Academy');
+INSERT INTO team_roster(Player,Number,Class,Pos,Height,Weight,Hometown,High_School) VALUES ('Jack Salt',33,'SR','C','6-10',250,'Auckland New Zealand','Westlake HS');
+INSERT INTO team_roster(Player,Number,Class,Pos,Height,Weight,Hometown,High_School) VALUES ('Kihei Clark',0,'FR','G','5-9',155,'Woodland Hills CA','Taft Charter HS');
+INSERT INTO team_roster(Player,Number,Class,Pos,Height,Weight,Hometown,High_School) VALUES ('Kody Stattmann',23,'FR','G','6-7',187,'Bentley Park Australia',NULL);
+INSERT INTO team_roster(Player,Number,Class,Pos,Height,Weight,Hometown,High_School) VALUES ('Marco Anthony',24,'SO','G','6-4',212,'San Antonio TX','Oliver Wendell Holmes High School');
+INSERT INTO team_roster(Player,Number,Class,Pos,Height,Weight,Hometown,High_School) VALUES ('Grant Kersey',1,'FR','G',NULL,NULL,'Charlottesville VA',NULL);
+INSERT INTO team_roster(Player,Number,Class,Pos,Height,Weight,Hometown,High_School) VALUES ('Austin Katstra',45,'SO','F','6-6',217,'Charlottesville VA','Albemarle High School');
+INSERT INTO team_roster(Player,Number,Class,Pos,Height,Weight,Hometown,High_School) VALUES ('Jayden Nixon',10,'FR','G','6-3',189,'Charlottesville VA','Choate Rosemary Hall HS');
+INSERT INTO team_roster(Player,Number,Class,Pos,Height,Weight,Hometown,High_School) VALUES ('Francesco Badocchi',1,'FR','F','6-7',205,'Milan Italy',NULL);
+INSERT INTO team_roster(Player,Number,Class,Pos,Height,Weight,Hometown,High_School) VALUES ('Francisco Caffaro',22,'FR','C','7-0',233,'El Trebo Argentina','NBA Global Academy');
