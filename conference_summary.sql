c626864d2db9ef9ccd29f4766041f77876a5676e
anna-thilly <31319616+anna-thilly@users.noreply.github.com>
2019-02-27 15:29:55 -0500

    conference summary sql upload

---
+++ conference_summary.sql
@@ -0,0 +1,42 @@
+CREATE TABLE conference_summary(
+   Rk                   INTEGER  NOT NULL PRIMARY KEY 
+  ,Conference           VARCHAR(34) NOT NULL
+  ,Schls                INTEGER  NOT NULL
+  ,W                    INTEGER  NOT NULL
+  ,L                    INTEGER  NOT NULL
+  ,WL                   NUMERIC(4,3) NOT NULL
+  ,SRS                  NUMERIC(6,2) NOT NULL
+  ,SOS                  NUMERIC(5,2) NOT NULL
+);
+INSERT INTO conference_summary(Rk,Conference,Schls,W,L,WL,SRS,SOS) VALUES (1,'Big Ten Conference',14,238,149,.615,15.86,10.38);
+INSERT INTO conference_summary(Rk,Conference,Schls,W,L,WL,SRS,SOS) VALUES (2,'Atlantic Coast Conference',15,260,152,.631,15.17,8.62);
+INSERT INTO conference_summary(Rk,Conference,Schls,W,L,WL,SRS,SOS) VALUES (3,'Big 12 Conference',10,173,103,.627,14.65,9.58);
+INSERT INTO conference_summary(Rk,Conference,Schls,W,L,WL,SRS,SOS) VALUES (4,'Southeastern Conference',14,233,151,.607,13.28,8.73);
+INSERT INTO conference_summary(Rk,Conference,Schls,W,L,WL,SRS,SOS) VALUES (5,'Big East Conference',10,169,106,.615,10.86,6.96);
+INSERT INTO conference_summary(Rk,Conference,Schls,W,L,WL,SRS,SOS) VALUES (6,'Pacific-12 Conference',12,180,146,.552,7.33,4.95);
+INSERT INTO conference_summary(Rk,Conference,Schls,W,L,WL,SRS,SOS) VALUES (7,'American Athletic Conference',12,194,129,.601,6.82,3.22);
+INSERT INTO conference_summary(Rk,Conference,Schls,W,L,WL,SRS,SOS) VALUES (8,'West Coast Conference',10,168,120,.583,5.58,2.20);
+INSERT INTO conference_summary(Rk,Conference,Schls,W,L,WL,SRS,SOS) VALUES (9,'Mid-American Conference',12,196,139,.585,3.58,1.58);
+INSERT INTO conference_summary(Rk,Conference,Schls,W,L,WL,SRS,SOS) VALUES (10,'Mountain West Conference',11,155,145,.517,2.03,1.31);
+INSERT INTO conference_summary(Rk,Conference,Schls,W,L,WL,SRS,SOS) VALUES (11,'Atlantic 10 Conference',14,197,185,.516,1.92,0.98);
+INSERT INTO conference_summary(Rk,Conference,Schls,W,L,WL,SRS,SOS) VALUES (12,'Missouri Valley Conference',10,152,137,.526,-0.27,0.11);
+INSERT INTO conference_summary(Rk,Conference,Schls,W,L,WL,SRS,SOS) VALUES (13,'Southern Conference',10,158,128,.552,-0.28,-0.55);
+INSERT INTO conference_summary(Rk,Conference,Schls,W,L,WL,SRS,SOS) VALUES (14,'Ivy Group',8,111,86,.563,-1.11,-1.54);
+INSERT INTO conference_summary(Rk,Conference,Schls,W,L,WL,SRS,SOS) VALUES (15,'Conference USA',14,206,179,.535,-1.23,-0.68);
+INSERT INTO conference_summary(Rk,Conference,Schls,W,L,WL,SRS,SOS) VALUES (16,'Sun Belt Conference',12,170,155,.523,-1.35,-0.77);
+INSERT INTO conference_summary(Rk,Conference,Schls,W,L,WL,SRS,SOS) VALUES (17,'Western Athletic Conference',9,135,114,.542,-2.37,-1.80);
+INSERT INTO conference_summary(Rk,Conference,Schls,W,L,WL,SRS,SOS) VALUES (18,'Horizon League',10,138,153,.474,-3.30,-1.58);
+INSERT INTO conference_summary(Rk,Conference,Schls,W,L,WL,SRS,SOS) VALUES (19,'Colonial Athletic Association',10,145,145,.500,-4.11,-2.81);
+INSERT INTO conference_summary(Rk,Conference,Schls,W,L,WL,SRS,SOS) VALUES (20,'Big West Conference',9,118,127,.482,-4.20,-2.08);
+INSERT INTO conference_summary(Rk,Conference,Schls,W,L,WL,SRS,SOS) VALUES (21,'Summit League',9,120,133,.474,-5.12,-2.90);
+INSERT INTO conference_summary(Rk,Conference,Schls,W,L,WL,SRS,SOS) VALUES (22,'Patriot League',10,138,143,.491,-5.31,-4.32);
+INSERT INTO conference_summary(Rk,Conference,Schls,W,L,WL,SRS,SOS) VALUES (23,'Atlantic Sun Conference',9,128,141,.476,-5.36,-2.81);
+INSERT INTO conference_summary(Rk,Conference,Schls,W,L,WL,SRS,SOS) VALUES (24,'Ohio Valley Conference',12,169,171,.497,-5.80,-3.81);
+INSERT INTO conference_summary(Rk,Conference,Schls,W,L,WL,SRS,SOS) VALUES (25,'Big South Conference',11,154,157,.495,-6.05,-4.15);
+INSERT INTO conference_summary(Rk,Conference,Schls,W,L,WL,SRS,SOS) VALUES (26,'America East Conference',9,118,134,.468,-7.62,-4.89);
+INSERT INTO conference_summary(Rk,Conference,Schls,W,L,WL,SRS,SOS) VALUES (27,'Big Sky Conference',11,138,153,.474,-7.97,-5.13);
+INSERT INTO conference_summary(Rk,Conference,Schls,W,L,WL,SRS,SOS) VALUES (28,'Metro Atlantic Athletic Conference',11,130,181,.418,-8.79,-6.02);
+INSERT INTO conference_summary(Rk,Conference,Schls,W,L,WL,SRS,SOS) VALUES (29,'Southland Conference',13,167,183,.477,-9.47,-5.03);
+INSERT INTO conference_summary(Rk,Conference,Schls,W,L,WL,SRS,SOS) VALUES (30,'Northeast Conference',10,129,153,.457,-10.34,-6.87);
+INSERT INTO conference_summary(Rk,Conference,Schls,W,L,WL,SRS,SOS) VALUES (31,'Southwest Athletic Conference',10,101,172,.370,-14.52,-7.03);
+INSERT INTO conference_summary(Rk,Conference,Schls,W,L,WL,SRS,SOS) VALUES (32,'Mid-Eastern Athletic Conference',12,127,212,.375,-15.68,-8.01);
