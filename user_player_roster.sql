7b8e14c0e7e6f555730a348ef38ad50bab6c76cf
derins <ds5bb@virginia.edu>
2019-03-02 18:57:44 -0500

    user team and user player roster working

--- user_player_roster.sql
+++ user_player_roster.sql
@@ -1,16 +1,16 @@
 CREATE TABLE user_player_roster(
 	userID			INT NOT NULL PRIMARY KEY
-	,Player      	VARCHAR(30) NOT NULL
-  	
+	,Player1      	VARCHAR(30) NOT NULL
+	,Player2     	VARCHAR(30) 
+	,Player3     	VARCHAR(30) 
+	,Player4     	VARCHAR(30) 
+	,Player5     	VARCHAR(30) 
 );
 
-INSERT INTO user_team_roster VALUES (1, 'Kyle Guy');
-INSERT INTO user_team_roster VALUES (1, 'Jack Salt');
-INSERT INTO user_team_roster VALUES (2, 'Jack Salt');
-INSERT INTO user_team_roster VALUES (2, 'Kody Stattmann');
-INSERT INTO user_team_roster VALUES (1, 'Kody Stattmann');
-INSERT INTO user_team_roster VALUES (4, 'Ty Jerome');
-INSERT INTO user_team_roster VALUES (5, 'Ty Jerome');
-INSERT INTO user_team_roster VALUES (3, 'Jayden Nixon');
-INSERT INTO user_team_roster VALUES (3, 'Kyle Guy');
-INSERT INTO user_team_roster VALUES (6, 'Jayden Nixon');
+INSERT INTO user_player_roster VALUES (1, 'Kyle Guy', 'Jack Salt', 'Kody Stattmann', 'Ty Jerome', 'Jayden Nixon');
+INSERT INTO user_player_roster VALUES (2, 'Jack Salt', 'Kody Stattmann', NULL, NULL, NULL);
+INSERT INTO user_player_roster VALUES (3, 'Jayden Nixon', NULL, NULL, NULL, NULL);
+INSERT INTO user_player_roster VALUES (4, 'Ty Jerome', 'Jayden Nixon', 'Mamadi Diakite', 'Jack Salt', NULL);
+INSERT INTO user_player_roster VALUES (5, 'Ty Jerome', 'Mamadi Diakite', 'Jack Salt', NULL, NULL);
+INSERT INTO user_player_roster VALUES (6, 'Jayden Nixon', 'Jay Huff', 'Marco Anthony', 'Francesco Badocchi', NULL);
+INSERT INTO user_player_roster VALUES (7, 'Jayden Nixon', NULL, NULL, NULL, NULL);
