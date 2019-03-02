98344f1cc2fc9000bb3a8824caeca8431cee14e7
derins <ds5bb@virginia.edu>
2019-03-02 18:39:54 -0500

    user_player and user_team roster tables done

---
+++ user_player_roster.sql
@@ -0,0 +1,16 @@
+CREATE TABLE user_player_roster(
+	userID			INT NOT NULL PRIMARY KEY
+	,Player      	VARCHAR(30) NOT NULL
+  	
+);
+
+INSERT INTO user_team_roster VALUES (1, 'Kyle Guy');
+INSERT INTO user_team_roster VALUES (1, 'Jack Salt');
+INSERT INTO user_team_roster VALUES (2, 'Jack Salt');
+INSERT INTO user_team_roster VALUES (2, 'Kody Stattmann');
+INSERT INTO user_team_roster VALUES (1, 'Kody Stattmann');
+INSERT INTO user_team_roster VALUES (4, 'Ty Jerome');
+INSERT INTO user_team_roster VALUES (5, 'Ty Jerome');
+INSERT INTO user_team_roster VALUES (3, 'Jayden Nixon');
+INSERT INTO user_team_roster VALUES (3, 'Kyle Guy');
+INSERT INTO user_team_roster VALUES (6, 'Jayden Nixon');
---
+++ user_team_roster.sql
@@ -0,0 +1,16 @@
+CREATE TABLE user_team_roster(
+	userID			INT NOT NULL PRIMARY KEY
+	,Team       	VARCHAR(30) NOT NULL
+  	
+);
+
+INSERT INTO user_team_roster VALUES (1, 'University of Virginia');
+INSERT INTO user_team_roster VALUES (1, 'Duke University');
+INSERT INTO user_team_roster VALUES (2, 'University of Virginia');
+INSERT INTO user_team_roster VALUES (2, 'Florida State University');
+INSERT INTO user_team_roster VALUES (1, 'Florida State University');
+INSERT INTO user_team_roster VALUES (4, 'University of Virginia');
+INSERT INTO user_team_roster VALUES (5, 'North Carolina State University');
+INSERT INTO user_team_roster VALUES (3, 'Wake Forest University');
+INSERT INTO user_team_roster VALUES (3, 'University of Virginia');
+INSERT INTO user_team_roster VALUES (6, 'Virginia Tech University');
