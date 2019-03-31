c095e6148696a6de816595214628684b155e8f0b
amobin19 <aymamob@gmail.com>
2019-03-31 15:19:26 -0400

    modified stuff in register and login pages

--- registration.php
+++ registration.php
@@ -6,15 +6,21 @@
     {
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
-    // Form the SQL query (an INSERT query)
-    $sql="INSERT INTO Users (userID, name, password, email, birthday)
-    VALUES
-    ('$_POST[idnum]','$_POST[name]','$_POST[password]','$_POST[email]','$_POST[birthday]')";
 
-    if (!mysqli_query($con,$sql))
-    {
-        die('Error: ' . mysqli_error($con));
+    if ($_POST["password"] === $_POST["password2"]) {
+        // Form the SQL query (an INSERT query)
+        $sql="INSERT INTO Users (userID, name, password, email, birthday)
+        VALUES
+        ('$_POST[idnum]','$_POST[name]','$_POST[password]','$_POST[email]','$_POST[birthday]')";
+
+        if (!mysqli_query($con,$sql))
+        {
+            die('Error: ' . mysqli_error($con));
+        }
+        header("Location: successReg.html");
+        mysqli_close($con);
     }
-    echo "1 record added"; // Output to user
-    mysqli_close($con);
+    else{
+        echo "Passwords do not match, failed to create a new account.";
+    }
 ?>
