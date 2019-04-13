f1ad5d59697c1d0bac586d1ac48d4ab2407788e9
oz5ed <48068245+oz5ed@users.noreply.github.com>
2019-04-13 13:06:16 -0400

    Update headCoaches.sql

--- db_tables/headCoaches.sql
+++ db_tables/headCoaches.sql
@@ -1,52 +1,41 @@
-/* Let's split this file up into sections, where each section
-is the code designed to create one of our 10 tables. */
-
-/* TODO:
-	player(name, height, age, school, school_year, hometown, weight, player_number)
-	player_stats(points/game, assists/game, rebounds/game, steals/game, blocks/game, fieldgoal%, 3pt%, TO/game, FreeThrow%)
-	head_coach(name, team, conference, games_won, games_lost)
-	team(wins, loss, conference, points_for, points_against, head_coach, ap_rank)
-	user_roster(teams, players)
-	users(name, password, user_id, email, birthday)
-	conference(rank, team, conf_wins, conf_losses, win_lost_percentage)
-	schedule(game, ...)
-*/
-
-
-/* SECTION X: 20 of the greatest head coaches */
-
 CREATE TABLE headCoach(
-   Name        VARCHAR(16) NOT NULL PRIMARY KEY
-  ,year_from   INTEGER  NOT NULL
-  ,year_to     INTEGER  NOT NULL
-  ,Last_School VARCHAR(15) NOT NULL
-  ,total_years INTEGER  NOT NULL
-  ,Games       INTEGER  NOT NULL
-  ,Wins        INTEGER  NOT NULL
-  ,Losses      INTEGER  NOT NULL
-  ,Ties        INTEGER  NOT NULL
-  ,Percent     NUMERIC(4,3) NOT NULL
+   Coach           VARCHAR(30) NOT NULL PRIMARY KEY
+  ,School          VARCHAR(30)
+  ,Conference      VARCHAR(30)
+  ,Sea_W           VARCHAR(30)
+  ,Sea_L           VARCHAR(30)
+  ,Sea_WL          VARCHAR(30)
+  ,AP_Pre          VARCHAR(30)
+  ,AP_Post         VARCHAR(30)
+  ,NCAA_Tournament VARCHAR(30)
+  ,Since           VARCHAR(30)
+  ,Sk_W            VARCHAR(30)
+  ,Sk_L            VARCHAR(30)
+  ,Sk_WL           VARCHAR(30)
+  ,Sk_NCAA         VARCHAR(30)
+  ,Sk_S16          VARCHAR(30)
+  ,Sk_FF           VARCHAR(30)
+  ,Sk_Chmp         VARCHAR(30)
+  ,W               VARCHAR(30)
+  ,L               VARCHAR(30)
+  ,WL              VARCHAR(30)
+  ,NCAA            VARCHAR(30)
+  ,S16             VARCHAR(30)
+  ,FF              VARCHAR(30)
+  ,Chmp            VARCHAR(30)
 );
-
-INSERT INTO headCoach(Name,year_from,year_to,Last_School,total_years,Games,Wins,Losses,Ties,Percent) VALUES ('Dino Babers',2014,2018,'Syracuse',5,64,36,28,0,.563);
-INSERT INTO headCoach(Name,year_from,year_to,Last_School,total_years,Games,Wins,Losses,Ties,Percent) VALUES ('Joe Bach',1934,1934,'Duquesne',1,10,8,2,0,.800);
-INSERT INTO headCoach(Name,year_from,year_to,Last_School,total_years,Games,Wins,Losses,Ties,Percent) VALUES ('Charlie Bachman',1919,1946,'Michigan State',27,234,132,80,22,.611);
-INSERT INTO headCoach(Name,year_from,year_to,Last_School,total_years,Games,Wins,Losses,Ties,Percent) VALUES ('Walter Bachman',1905,1906,'Texas A&M',2,16,13,3,0,.813);
-INSERT INTO headCoach(Name,year_from,year_to,Last_School,total_years,Games,Wins,Losses,Ties,Percent) VALUES ('A.H. Badenoch',1918,1918,'Stanford',1,4,0,4,0,.000);
-INSERT INTO headCoach(Name,year_from,year_to,Last_School,total_years,Games,Wins,Losses,Ties,Percent) VALUES ('Chuck Baer',1945,1950,'Detroit Mercy',6,57,35,21,1,.623);
-INSERT INTO headCoach(Name,year_from,year_to,Last_School,total_years,Games,Wins,Losses,Ties,Percent) VALUES ('Kent Baer',2004,2012,'San Jose State',2,2,1,1,0,.500);
-INSERT INTO headCoach(Name,year_from,year_to,Last_School,total_years,Games,Wins,Losses,Ties,Percent) VALUES ('Enoch Bagshaw',1921,1929,'Washington',9,91,64,21,6,.736);
-INSERT INTO headCoach(Name,year_from,year_to,Last_School,total_years,Games,Wins,Losses,Ties,Percent) VALUES ('Charlie Bailey',1986,1999,'Texas-El Paso',10,106,31,73,2,.302);
-INSERT INTO headCoach(Name,year_from,year_to,Last_School,total_years,Games,Wins,Losses,Ties,Percent) VALUES ('David Bailiff',2007,2017,'Rice',11,137,57,80,0,.416);
-INSERT INTO headCoach(Name,year_from,year_to,Last_School,total_years,Games,Wins,Losses,Ties,Percent) VALUES ('A.G. Baillet',1892,1892,'Tufts',1,10,8,2,0,.800);
-INSERT INTO headCoach(Name,year_from,year_to,Last_School,total_years,Games,Wins,Losses,Ties,Percent) VALUES ('Art Baker',1973,1988,'East Carolina',13,143,64,74,5,.465);
-INSERT INTO headCoach(Name,year_from,year_to,Last_School,total_years,Games,Wins,Losses,Ties,Percent) VALUES ('Edward Baker',1940,1940,'Carnegie Mellon',1,8,3,5,0,.375);
-INSERT INTO headCoach(Name,year_from,year_to,Last_School,total_years,Games,Wins,Losses,Ties,Percent) VALUES ('John Baker',1942,1952,'Denver',6,61,23,36,2,.393);
-INSERT INTO headCoach(Name,year_from,year_to,Last_School,total_years,Games,Wins,Losses,Ties,Percent) VALUES ('Dave Baldwin',1997,2014,'Colorado State',5,46,18,28,0,.391);
-INSERT INTO headCoach(Name,year_from,year_to,Last_School,total_years,Games,Wins,Losses,Ties,Percent) VALUES ('James Baldwin',1922,1927,'Wake Forest',4,38,16,17,5,.487);
-INSERT INTO headCoach(Name,year_from,year_to,Last_School,total_years,Games,Wins,Losses,Ties,Percent) VALUES ('Jerry Baldwin',1999,2001,'Louisiana',3,33,6,27,0,.182);
-INSERT INTO headCoach(Name,year_from,year_to,Last_School,total_years,Games,Wins,Losses,Ties,Percent) VALUES ('D.M. Balliet',1893,1901,'Purdue',4,34,22,10,2,.676);
-INSERT INTO headCoach(Name,year_from,year_to,Last_School,total_years,Games,Wins,Losses,Ties,Percent) VALUES ('Ted Bank',1935,1940,'Idaho',6,54,18,32,4,.370);
-INSERT INTO headCoach(Name,year_from,year_to,Last_School,total_years,Games,Wins,Losses,Ties,Percent) VALUES ('Laurence Bankart',1910,1916,'Colgate',5,38,28,7,3,.776);
-
-/* End head coaches */
+INSERT INTO headCoach(Coach,School,Conference,Sea_W,Sea_L,Sea_WL,AP_Pre,AP_Post,NCAA_Tournament,Since,Sk_W,Sk_L,Sk_WL,Sk_NCAA,Sk_S16,Sk_FF,Sk_Chmp,W,L,WL,NCAA,S16,FF,Chmp) VALUES ('Jim Christian','Boston College','ACC','14','17','.452',NULL,NULL,NULL,'2014-15','62','100','.383',NULL,NULL,NULL,NULL,'304','254','.545','2',NULL,NULL,NULL);
+INSERT INTO headCoach(Coach,School,Conference,Sea_W,Sea_L,Sea_WL,AP_Pre,AP_Post,NCAA_Tournament,Since,Sk_W,Sk_L,Sk_WL,Sk_NCAA,Sk_S16,Sk_FF,Sk_Chmp,W,L,WL,NCAA,S16,FF,Chmp) VALUES ('Brad Brownell','Clemson','ACC','20','14','.588','22',NULL,NULL,'2010-11','169','127','.571','2','1',NULL,NULL,'336','212','.613','5','1',NULL,NULL);
+INSERT INTO headCoach(Coach,School,Conference,Sea_W,Sea_L,Sea_WL,AP_Pre,AP_Post,NCAA_Tournament,Since,Sk_W,Sk_L,Sk_WL,Sk_NCAA,Sk_S16,Sk_FF,Sk_Chmp,W,L,WL,NCAA,S16,FF,Chmp) VALUES ('Mike Krzyzewski','Duke','ACC','32','6','.842','4','1','Lost Regional Final','1980-81','1059','285','.788','35','25','12','5','1132','344','.767','35','25','12','5');
+INSERT INTO headCoach(Coach,School,Conference,Sea_W,Sea_L,Sea_WL,AP_Pre,AP_Post,NCAA_Tournament,Since,Sk_W,Sk_L,Sk_WL,Sk_NCAA,Sk_S16,Sk_FF,Sk_Chmp,W,L,WL,NCAA,S16,FF,Chmp) VALUES ('Leonard Hamilton*','Florida State','ACC','29','8','.784','17','10','Lost Regional Semifinal','2002-03','356','216','.622','7','3',NULL,NULL,'556','426','.566','10','4',NULL,NULL);
+INSERT INTO headCoach(Coach,School,Conference,Sea_W,Sea_L,Sea_WL,AP_Pre,AP_Post,NCAA_Tournament,Since,Sk_W,Sk_L,Sk_WL,Sk_NCAA,Sk_S16,Sk_FF,Sk_Chmp,W,L,WL,NCAA,S16,FF,Chmp) VALUES ('Josh Pastner','Georgia Tech','ACC','14','18','.438',NULL,NULL,NULL,'2016-17','48','53','.475',NULL,NULL,NULL,NULL,'215','126','.630','4',NULL,NULL,NULL);
+INSERT INTO headCoach(Coach,School,Conference,Sea_W,Sea_L,Sea_WL,AP_Pre,AP_Post,NCAA_Tournament,Since,Sk_W,Sk_L,Sk_WL,Sk_NCAA,Sk_S16,Sk_FF,Sk_Chmp,W,L,WL,NCAA,S16,FF,Chmp) VALUES ('Chris Mack','Louisville','ACC','20','14','.588',NULL,NULL,'Lost First Round','2018-19','20','14','.588','1',NULL,NULL,NULL,'235','111','.679','9','4',NULL,NULL);
+INSERT INTO headCoach(Coach,School,Conference,Sea_W,Sea_L,Sea_WL,AP_Pre,AP_Post,NCAA_Tournament,Since,Sk_W,Sk_L,Sk_WL,Sk_NCAA,Sk_S16,Sk_FF,Sk_Chmp,W,L,WL,NCAA,S16,FF,Chmp) VALUES ('Jim Larranaga','Miami (FL)','ACC','14','18','.438',NULL,NULL,NULL,'2011-12','175','97','.643','4','2',NULL,NULL,'617','406','.603','9','3','1',NULL);
+INSERT INTO headCoach(Coach,School,Conference,Sea_W,Sea_L,Sea_WL,AP_Pre,AP_Post,NCAA_Tournament,Since,Sk_W,Sk_L,Sk_WL,Sk_NCAA,Sk_S16,Sk_FF,Sk_Chmp,W,L,WL,NCAA,S16,FF,Chmp) VALUES ('Roy Williams','UNC','ACC','29','7','.806','8','3','Lost Regional Semifinal','2003-04','453','133','.773','15','10','5','3','871','234','.788','29','19','9','3');
+INSERT INTO headCoach(Coach,School,Conference,Sea_W,Sea_L,Sea_WL,AP_Pre,AP_Post,NCAA_Tournament,Since,Sk_W,Sk_L,Sk_WL,Sk_NCAA,Sk_S16,Sk_FF,Sk_Chmp,W,L,WL,NCAA,S16,FF,Chmp) VALUES ('Kevin Keatts','NC State','ACC','24','12','.667',NULL,NULL,NULL,'2017-18','45','24','.652','1',NULL,NULL,NULL,'117','52','.692','3',NULL,NULL,NULL);
+INSERT INTO headCoach(Coach,School,Conference,Sea_W,Sea_L,Sea_WL,AP_Pre,AP_Post,NCAA_Tournament,Since,Sk_W,Sk_L,Sk_WL,Sk_NCAA,Sk_S16,Sk_FF,Sk_Chmp,W,L,WL,NCAA,S16,FF,Chmp) VALUES ('Mike Brey','Notre Dame','ACC','14','19','.424',NULL,NULL,NULL,'2000-01','417','221','.654','12','3',NULL,NULL,'516','273','.654','14','3',NULL,NULL);
+INSERT INTO headCoach(Coach,School,Conference,Sea_W,Sea_L,Sea_WL,AP_Pre,AP_Post,NCAA_Tournament,Since,Sk_W,Sk_L,Sk_WL,Sk_NCAA,Sk_S16,Sk_FF,Sk_Chmp,W,L,WL,NCAA,S16,FF,Chmp) VALUES ('Jeff Capel*','Pitt','ACC','14','19','.424',NULL,NULL,NULL,'2018-19','14','19','.424',NULL,NULL,NULL,NULL,'189','129','.594','3','1',NULL,NULL);
+INSERT INTO headCoach(Coach,School,Conference,Sea_W,Sea_L,Sea_WL,AP_Pre,AP_Post,NCAA_Tournament,Since,Sk_W,Sk_L,Sk_WL,Sk_NCAA,Sk_S16,Sk_FF,Sk_Chmp,W,L,WL,NCAA,S16,FF,Chmp) VALUES ('Jim Boeheim*','Syracuse','ACC','20','14','.588','16',NULL,'Lost First Round','1976-77','1047','385','.731','34','18','5','1','1047','385','.731','34','18','5','1');
+INSERT INTO headCoach(Coach,School,Conference,Sea_W,Sea_L,Sea_WL,AP_Pre,AP_Post,NCAA_Tournament,Since,Sk_W,Sk_L,Sk_WL,Sk_NCAA,Sk_S16,Sk_FF,Sk_Chmp,W,L,WL,NCAA,S16,FF,Chmp) VALUES ('Tony Bennett','Virginia','ACC','35','3','.921','5','2','Won National Final','2009-10','254','89','.741','7','3','1','1','323','122','.726','9','4','1','1');
+INSERT INTO headCoach(Coach,School,Conference,Sea_W,Sea_L,Sea_WL,AP_Pre,AP_Post,NCAA_Tournament,Since,Sk_W,Sk_L,Sk_WL,Sk_NCAA,Sk_S16,Sk_FF,Sk_Chmp,W,L,WL,NCAA,S16,FF,Chmp) VALUES ('Buzz Williams','Virginia Tech','ACC','26','9','.743','15','16','Lost Regional Semifinal','2014-15','100','69','.592','3','1',NULL,NULL,'253','155','.620','8','4',NULL,NULL);
+INSERT INTO headCoach(Coach,School,Conference,Sea_W,Sea_L,Sea_WL,AP_Pre,AP_Post,NCAA_Tournament,Since,Sk_W,Sk_L,Sk_WL,Sk_NCAA,Sk_S16,Sk_FF,Sk_Chmp,W,L,WL,NCAA,S16,FF,Chmp) VALUES ('Danny Manning','Wake Forest','ACC','11','20','.355',NULL,NULL,NULL,'2014-15','65','93','.411','1',NULL,NULL,NULL,'103','122','.458','2',NULL,NULL,NULL);
