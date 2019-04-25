22a1b65326eb478f2c957fe84cb0d7afa2f37821
anna-thilly <31319616+anna-thilly@users.noreply.github.com>
2019-04-24 20:40:10 -0400

    Add files via upload

---
+++ db_tables/tournament_mvp (1).sql
@@ -0,0 +1,33 @@
+CREATE TABLE IF NOT EXISTS tournament_mvp(
+   Season INTEGER  NOT NULL PRIMARY KEY 
+  ,Player VARCHAR(41) NOT NULL
+  ,School VARCHAR(24) NOT NULL
+  ,G      INTEGER  NOT NULL
+  ,GS     INTEGER 
+  ,PTS    INTEGER  NOT NULL
+  ,TRB    INTEGER 
+  ,AST    INTEGER 
+  ,STL    INTEGER 
+  ,BLK    INTEGER 
+  ,FG     NUMERIC(4,3)
+);
+INSERT INTO tournament_mvp(Season,Player,School,G,GS,PTS,TRB,AST,STL,BLK,FG) VALUES (2019,'Kyle Guy','Virginia',6,6,86,31,9,3,1,.349);
+INSERT INTO tournament_mvp(Season,Player,School,G,GS,PTS,TRB,AST,STL,BLK,FG) VALUES (2018,'Donte DiVincenzo','Villanova',6,0,90,33,22,2,3,.547);
+INSERT INTO tournament_mvp(Season,Player,School,G,GS,PTS,TRB,AST,STL,BLK,FG) VALUES (2017,'Joel Berry','North Carolina',6,6,83,14,19,8,1,.304);
+INSERT INTO tournament_mvp(Season,Player,School,G,GS,PTS,TRB,AST,STL,BLK,FG) VALUES (2016,'Ryan Arcidiacono','Villanova',6,6,95,13,18,7,0,.660);
+INSERT INTO tournament_mvp(Season,Player,School,G,GS,PTS,TRB,AST,STL,BLK,FG) VALUES (2015,'Tyus Jones','Duke',6,6,78,19,27,10,1,.424);
+INSERT INTO tournament_mvp(Season,Player,School,G,GS,PTS,TRB,AST,STL,BLK,FG) VALUES (2014,'Shabazz Napier','Connecticut',6,6,127,33,27,15,0,.463);
+INSERT INTO tournament_mvp(Season,Player,School,G,GS,PTS,TRB,AST,STL,BLK,FG) VALUES (2013,'Luke Hancock','Louisville',6,0,69,10,9,6,2,.633);
+INSERT INTO tournament_mvp(Season,Player,School,G,GS,PTS,TRB,AST,STL,BLK,FG) VALUES (2012,'Anthony Davis','Kentucky',6,6,82,74,18,7,29,.510);
+INSERT INTO tournament_mvp(Season,Player,School,G,GS,PTS,TRB,AST,STL,BLK,FG) VALUES (2011,'Kemba Walker','Connecticut',6,6,141,36,34,9,1,.402);
+INSERT INTO tournament_mvp(Season,Player,School,G,GS,PTS,TRB,AST,STL,BLK,FG) VALUES (2010,'Kyle Singler','Duke',6,6,108,44,16,5,4,.443);
+INSERT INTO tournament_mvp(Season,Player,School,G,GS,PTS,TRB,AST,STL,BLK,FG) VALUES (2009,'Wayne Ellington','North Carolina',6,6,115,34,16,2,2,.550);
+INSERT INTO tournament_mvp(Season,Player,School,G,GS,PTS,TRB,AST,STL,BLK,FG) VALUES (2008,'Mario Chalmers','Kansas',6,6,89,19,18,17,2,.492);
+INSERT INTO tournament_mvp(Season,Player,School,G,GS,PTS,TRB,AST,STL,BLK,FG) VALUES (2007,'Corey Brewer','Florida',6,6,95,33,13,10,6,.448);
+INSERT INTO tournament_mvp(Season,Player,School,G,GS,PTS,TRB,AST,STL,BLK,FG) VALUES (2006,'Joakim Noah','Florida',6,6,97,57,19,8,29,.550);
+INSERT INTO tournament_mvp(Season,Player,School,G,GS,PTS,TRB,AST,STL,BLK,FG) VALUES (2005,'Sean May','North Carolina',6,6,134,64,11,5,5,.667);
+INSERT INTO tournament_mvp(Season,Player,School,G,GS,PTS,TRB,AST,STL,BLK,FG) VALUES (2004,'Emeka Okafor','Connecticut',6,6,81,68,9,5,13,.571);
+INSERT INTO tournament_mvp(Season,Player,School,G,GS,PTS,TRB,AST,STL,BLK,FG) VALUES (2003,'Carmelo Anthony','Syracuse',6,6,121,59,15,11,4,.475);
+INSERT INTO tournament_mvp(Season,Player,School,G,GS,PTS,TRB,AST,STL,BLK,FG) VALUES (2002,'Juan Dixon','Maryland',6,6,155,23,19,12,0,.542);
+INSERT INTO tournament_mvp(Season,Player,School,G,GS,PTS,TRB,AST,STL,BLK,FG) VALUES (2001,'Shane Battier','Duke',6,6,135,61,14,12,16,.477);
+INSERT INTO tournament_mvp(Season,Player,School,G,GS,PTS,TRB,AST,STL,BLK,FG) VALUES (2000,'Mateen Cleaves','Michigan State',6,6,85,12,27,7,2,.418);
