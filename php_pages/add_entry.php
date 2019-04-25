ebaa49e610d84a72d3ddcac4d1697ac554e6ace0
anna-thilly <atn2a@virginia.edu>
2019-04-24 21:09:52 -0400

    updates

--- php_pages/add_entry.php
+++ php_pages/add_entry.php
@@ -1,5 +1,24 @@
+<html>
+  <body>
+<head>
 
+  <link rel="stylesheet" href="../styles.css">
+
+</head>
+
+<div class="topnav">
+  <a class="active" href="../php_pages/login.php">Home</a>
+  <a href="../db_interaction/landing.php">My Team</a>
+  <a href="">Stats Lab</a>
+  <a href="../html_pages/add_entry.html">Add Players</a>
+
+</div>
+
+    <h2>Add a Player!</h2>
+    <BR>
 <?php
+	session_start();
+	if($_SESSION["admin"] ==1){
 	include_once("./library.php"); // To connect to the database
 	$con = new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE);
 	// Check connection
@@ -13,5 +32,10 @@
 		}
 	echo "1 record added"; // Output to user
 	mysqli_close($con);
+	}
+        else {
+	header('Location: /login.php');
+}
 ?>
-
+</body>
+</html>
